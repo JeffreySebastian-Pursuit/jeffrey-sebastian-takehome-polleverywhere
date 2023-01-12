722b1efbd4620ffa20f1d78ebf340b9292eb882f
Jeffrey <jeffreysebastian@pursuit.org>
2023-01-12 18:35:19 -0500

    create app.js server.js gitignore and env file

---
+++ app.js
@@ -0,0 +1,21 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("Welcome to my page");
+  });
+
+  app.get("*", (req, res) => {
+    res.status(404).send("Page not found");
+  });
+
+
+  module.exports = app;
